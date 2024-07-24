1b842d5a8765e32ae81d80cd0311ba8fb784d0b8
klaviwka <klaviwka@mail.ru>
2024-07-24 23:16:34 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,12 @@
+my_dict = {'apple': 5, 'bananas': 12, 'mandarin': 32}
+print(my_dict)
+print(my_dict.get('apple'))
+print(my_dict.get('ages'))
+my_dict.update({'orange': 7, 'tomatoes': 14})
+del my_dict['bananas']
+print(my_dict.get('bananas'))
+print(my_dict)
+my_set = {1, 4, 7, 'man', True, 7, 4, 1, 'man'}
+print(my_set)
+print(my_set.discard(4))
+print(my_set)
